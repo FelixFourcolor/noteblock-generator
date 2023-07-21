884b99a0aef63cf1485fefe77e1330289af21a7c
laptop <felix.fourcolor@gmail.com>
2023-07-21 11:34:56 -0700

    extend mappings to c1 - b7, to accommodate transposing

--- music.py
+++ music.py
@@ -1,49 +1,46 @@
-# mapping of pitch names to their values
-_octaves = [
-    # create the first octave
-    ["fs1", "g1", "gs1", "a1", "as1", "b1", "c2", "cs2", "d2", "ds2", "e2", "f2"]
-]
-for _ in range(5):
-    # dynamically extend to octave 7
+# MAPPING OF PITCH NAMES TO NUMERICAL VALUES
+# create first octave
+_first = ["c1", "cs1", "d1", "ds1", "e1", "f1", "fs1", "g1", "gs1", "a1", "as1", "b1"]
+_octaves = [_first]
+# dynamically extend to octave 7
+for _ in range(6):
     _octaves.append([p[:-1] + str(int(p[-1]) + 1) for p in _octaves[-1]])
-_PITCHES = {
-    # flatten and convert to dict
+# flatten and convert to dict
+PITCHES = {
     name: value
-    for value, name in enumerate(
-        [pitch for octave in _octaves for pitch in octave] + ["fs7"]
-    )
+    for value, name in enumerate([pitch for octave in _octaves for pitch in octave])
 }
-for name, value in dict(_PITCHES).items():
-    # extend accidentals
+# extend accidentals
+for name, value in dict(PITCHES).items():
     if name[-2] == "s":
         # double sharps
-        if value + 1 <= 72:
-            _PITCHES[name[:-1] + "s" + name[-1]] = value + 1
-            _PITCHES[name[:-2] + "x" + name[-1]] = value + 1
+        if value + 1 < 84:
+            PITCHES[name[:-1] + "s" + name[-1]] = value + 1
+            PITCHES[name[:-2] + "x" + name[-1]] = value + 1
     else:
         # flats
         if value - 1 >= 0:
-            _PITCHES[name[:-1] + "b" + name[-1]] = value - 1
+            PITCHES[name[:-1] + "b" + name[-1]] = value - 1
         # double flats
         if value - 2 >= 0:
-            _PITCHES[name[:-1] + "bb" + name[-1]] = value - 2
+            PITCHES[name[:-1] + "bb" + name[-1]] = value - 2
 
-# Mapping of instruments to their ranges
-_INSTRUMENTS = {
-    "bass": range(0, 24),
-    "didgeridoo": range(0, 24),
-    "guitar": range(12, 36),
-    "harp": range(24, 48),
-    "bit": range(24, 48),
-    "banjo": range(24, 48),
-    "iron_xylophone": range(24, 48),
-    "pling": range(24, 48),
-    "flute": range(36, 60),
-    "cow_bell": range(36, 60),
-    "bell": range(48, 72),
-    "xylophone": range(48, 72),
-    "chime": range(48, 72),
-    "basedrum": range(72),
-    "hat": range(72),
-    "snare": range(72),
+# MAPPING OF INSTRUMENTS TO NUMERICAL RANGES
+INSTRUMENTS = {
+    "bass": range(6, 30),
+    "didgeridoo": range(6, 30),
+    "guitar": range(18, 42),
+    "harp": range(30, 54),
+    "bit": range(30, 54),
+    "banjo": range(30, 54),
+    "iron_xylophone": range(30, 54),
+    "pling": range(30, 54),
+    "flute": range(42, 66),
+    "cow_bell": range(42, 66),
+    "bell": range(54, 78),
+    "xylophone": range(54, 78),
+    "chime": range(54, 78),
+    "basedrum": range(78),
+    "hat": range(78),
+    "snare": range(78),
 }
