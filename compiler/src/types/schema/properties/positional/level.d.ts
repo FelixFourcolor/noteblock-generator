7537fa07e56523751589095312ff4b61887cd558
Felix <felix.fourcolor@gmail.com>
2025-09-24 21:16:56 -0700

    fix dynamic, level, division schemas

--- compiler/src/types/schema/properties/positional/level.d.ts
+++ compiler/src/types/schema/properties/positional/level.d.ts
@@ -3,24 +3,16 @@ import type { Positional } from "../meta.ts";
 import type { Variable } from "../variable.ts";
 
 export type Level = Level.uniform | Level.variable;
-export interface ILevel {
-	level: Positional<Level>;
-}
 
 export namespace Level {
-	export type absolute = uniform.absolute | variable.absolute;
-	export type relative = uniform.relative | variable.relative;
-
 	export type uniform = uniform.absolute | uniform.relative;
 	export namespace uniform {
 		export type absolute = Int<0> | Re<"\\d+">;
 		export type relative = Re<"[+-]", Token<uniform.absolute>>;
 	}
+	export type variable = Variable<uniform | Re<"~">>;
+}
 
-	export type variable = variable.absolute | variable.relative;
-	export namespace variable {
-		export type absolute = Variable<uniform.absolute>;
-		export type relative = Variable<uniform | Re<"~">> &
-			Re<uniform.relative | Re<"~">>;
-	}
+export interface ILevel {
+	level: Positional<Level>;
 }
