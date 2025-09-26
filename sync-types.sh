e089a788077977db42f7e60b127d9f1a9511dc1b
Felix <felix.fourcolor@gmail.com>
2025-09-25 22:39:24 -0700

    update

--- sync-types.sh
+++ sync-types.sh
@@ -2,20 +2,10 @@
 
 ROOT=$(realpath "$(dirname "$0")")
 
-transpile() {
-    # must cd because typescript2python only lives inside compiler
-    cd "$ROOT/compiler"
-    local ts_file="src/api/types.d.ts"
-    npx --silent typescript2python "$ts_file" --strict
-}
+TS_FILE="$ROOT/compiler/src/api/types.d.ts"
+PY_FILE="$ROOT/generator/noteblock_generator/api/types.py"
 
-generate() {
-    cd "$ROOT/generator"
-    local py_file="noteblock_generator/api/types.py"
-    {
-        echo -e '"""Transpiled from TypeScript."""\n'
-        transpile
-    } > "$py_file"
-}
-
-generate
+{
+    echo -e '"""Transpiled from TypeScript."""\n'
+    npx --silent typescript2python "$TS_FILE" --strict
+} > "$PY_FILE"
