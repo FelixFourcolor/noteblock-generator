8ff49202fe01931cbdae1d2003c07001e659caa6
Felix <felix.fourcolor@gmail.com>
2025-10-26 21:45:28 -0700

    generator to accept input as a zip

--- generator/noteblock_generator/api/loader.py
+++ generator/noteblock_generator/api/loader.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from io import BytesIO
 from pathlib import Path
 from sys import stdin
+from zipfile import ZipFile, is_zipfile
 
 from msgspec import json
 
@@ -9,16 +11,40 @@ from .types import Building
 
 
 def load(path: Path | None) -> Building | None:
-    if data := _load_bytes(path):
-        return json.decode(data, type=Building)
+    if not (src := _load_source(path)):
+        return None
+
+    if not (data := _read_source(src)):
+        return None
+
+    return json.decode(data, type=Building)
 
 
-def _load_bytes(path: Path | None) -> bytes | None:
+def _load_source(path: Path | None) -> Path | BytesIO | None:
     if path:
-        with path.open("rb") as f:
-            return f.read()
+        return path
 
-    if not stdin.isatty():
-        # prevent infinite loop if user does something like `yes | nbg`
-        MAX_SIZE = 100 * 1024 * 1024  # 100 MB
-        return stdin.buffer.read(MAX_SIZE)
+    if stdin.isatty():
+        return None
+
+    # prevent an infinite loop in case user does something like "yes | nbg"
+    MAX_SIZE = 100 * 1024 * 1024  # 100 MB
+    return BytesIO(stdin.buffer.read(MAX_SIZE))
+
+
+def _read_source(src: Path | BytesIO) -> bytes | None:
+    if is_zipfile(src):
+        return _unzip(src)
+
+    if isinstance(src, Path):
+        return src.read_bytes()
+
+    return src.getvalue()
+
+
+def _unzip(src: Path | BytesIO) -> bytes:
+    with ZipFile(src) as zf:
+        files = [n for n in zf.namelist() if not n.endswith("/")]
+        if len(files) != 1:
+            raise ValueError
+        return zf.read(files[0])
