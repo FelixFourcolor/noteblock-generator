bdd17f769abbcf111f77ba7bc9d95b8d6b29ef78
Felix <felix.fourcolor@gmail.com>
2026-01-27 16:43:51 -0800

    format

--- compiler/src/types/helpers/array.d.ts
+++ compiler/src/types/helpers/array.d.ts
@@ -2,11 +2,12 @@
  * https://github.com/hackle/blog-rust/blob/master/sample/typescript-union-to-tuple-array
  */
 
-export type Tuplify<Union> = PickOne<Union> extends infer Last
-	? Exclude<Union, Last> extends never
-		? [Last]
-		: [...Tuplify<Exclude<Union, Last>>, Last]
-	: never;
+export type Tuplify<Union> =
+	PickOne<Union> extends infer Last
+		? Exclude<Union, Last> extends never
+			? [Last]
+			: [...Tuplify<Exclude<Union, Last>>, Last]
+		: never;
 
 type PickOne<T> = InferContra<InferContra<Contra<Contra<T>>>>;
 
